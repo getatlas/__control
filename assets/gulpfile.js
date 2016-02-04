43072de02e314bd219e4a3ed30606abeface7412
Thomas Kimura <thomas@kimuradigital.com>
2016-02-04 15:53:35 -0500

    Ready for first client hand over

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,43 +1,30 @@
-// sudo npm install --save-dev gulp-sass gulp-rename gulp-minify-css gulp-livereload gulp-styledown gulp-autoprefixer gulp-load-plugins gulp-autoprefixer gulp-sourcemaps gulp-htmlmin gulp-shell
-//
+// sudo npm install --save-dev gulp-sass gulp-rename gulp-minify-css gulp-livereload gulp-autoprefixer gulp-load-plugins gulp-autoprefixer gulp-htmlmin
+
 // DURING DEVELOPMENT USE:
 // gulp watch
 
-// MINIFY HTML FOR PRODUCTION:
-// gulp minifyHtml
 'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
-var livereload = require('gulp-livereload');
-var styledown = require('gulp-styledown');
 var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var htmlmin = require('gulp-htmlmin');
-var shell = require('gulp-shell');
-
-
-gulp.task('sass', function () {
-  return gulp.src('sass/style.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(minifyCSS())
-    .pipe(autoprefixer())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('css'))
-    .pipe(livereload());
-});
+var livereload = require('gulp-livereload');
+// var styledown = require('gulp-styledown');
+// var shell = require('gulp-shell');
 
 
 // gulp.task('build', shell.task([ 'jekyll serve' ]));
 
 
-gulp.task('minifyHtml', function() {
-  return gulp.src('_site/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('_site/'))
-});
+// gulp.task('minifyHtml', function() {
+//   return gulp.src('_site/*.html')
+//     .pipe(htmlmin({collapseWhitespace: true}))
+//     .pipe(gulp.dest('_site/'))
+// });
 
 
 gulp.task('watch', function() {
@@ -48,4 +35,15 @@ gulp.task('watch', function() {
   //   '_layouts/**/*.html',
   //   '_posts/**/*'
   // ], ['build']);
+});
+
+
+gulp.task('sass', function () {
+  return gulp.src('sass/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(minifyCSS())
+    .pipe(autoprefixer())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(livereload());
 });
